b8ec8afeecd8b2a46e9ff5deff5a3ea3078a718d
Joskemu <vadhelkemu098@gmail.com>
2025-02-16 11:01:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-// complete this js code
-function Person(name, age) {}
+// Person class
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-function Employee(name, age, jobTitle) {}
+  greet() {
+    return `Hello, my name is ${this.name}, I am ${this.age} years old.`;
+  }
+}
 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+// Employee class extending Person class
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    super(name, age);
+    this.jobTitle = jobTitle;
+  }
+
+  jobGreet() {
+    return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`;
+  }
+}
+
+// Function to create and greet a Person instance
+function createPerson() {
+  const person = new Person("Alice", 25);
+  const greetingMessage = person.greet();
+  document.getElementById("personGreeting").innerText = greetingMessage;
+}
+
+// Function to create and greet an Employee instance
+function createEmployee() {
+  const employee = new Employee("Bob", 30, "Manager");
+  const greetingMessage = employee.jobGreet();
+  document.getElementById("employeeGreeting").innerText = greetingMessage;
+}
